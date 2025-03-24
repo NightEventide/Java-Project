8300047a1b3a1a75d6330c861a84ff48167139fb
NightEventide <seanlee12312@gmail.com~>
2025-03-24 19:54:26 +0800

    First

--- src/javaproject/JavaProject.java
+++ src/javaproject/JavaProject.java
@@ -14,6 +14,7 @@ public class JavaProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("First Commit");
         // TODO code application logic here
     }
     
