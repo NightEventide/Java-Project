68dee74415088c30a75513e59a4ac701ed9fb067
NightEventide <seanlee12312@gmail.com~>
2025-03-24 19:56:49 +0800

    First

---
+++ JavaProject/src/javaproject/JavaProject.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaproject;
+
+/**
+ *
+ * @author seanl
+ */
+public class JavaProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("First Commit");
+        // TODO code application logic here
+    }
+    
+}
