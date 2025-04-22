2ca35c431425c136e48d7b87d73749692b25cc19
NightEventide <seanlee12312@gmail.com~>
2025-04-22 21:42:08 +0800

    Added DataStorage with Users

---
+++ JavaProject/src/Admin/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Admin;
+
+/**
+ *
+ * @author seanl
+ */
+public class NewClass {
+    
+        public NewClass() {
+        System.out.println("Stupid project");
+    }
+}
